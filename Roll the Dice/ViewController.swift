2c8f034476087ab7a4770a1a5ac08ecba5f218ee
Danyil Marchuk <zemero58@gmail.com>
2022-12-09 15:52:03 +0100

    We only set the scene with almost no code

--- Roll the Dice/ViewController.swift
+++ Roll the Dice/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//  IBO allows me to reference a UI element
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // WHO         WHAT     VALUE
+        diceImageView1.image = #imageLiteral(resourceName: "Dice 3")
+        diceImageView2.image = #imageLiteral(resourceName: "Dice 5")
+        diceImageView1.alpha = 0.5
+        
+        
     }
 
 
