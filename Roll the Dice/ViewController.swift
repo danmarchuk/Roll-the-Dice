4e2a3ef43a46c20c8c70f8017abd58b1a9bd6698
Danyil Marchuk <zemero58@gmail.com>
2022-12-09 14:56:44 +0100

    Initial Commit

---
+++ Roll the Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll the Dice
+//
+//  Created by Данік on 09/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
